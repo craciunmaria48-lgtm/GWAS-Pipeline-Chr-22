30734aee688329b79d8100a6b78cec4f03033b42
Craciun Maria Alexandra <craciunmaria48@gmail.com>
2026-04-22 22:01:36 +0300

    Update run_pipeline.sh

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -5,44 +5,48 @@
 #Author Craciun Maria Alexandra
 #==============================================================
 
-# 0. Create directory structure
-
-mkdir -p raw_data data_clean results
+#!/bin/bash
 
 echo "Starting GWAS Pipeline"
 
-# 1. Convert VCF to PLINK Binary Format
+# 1. Create directories
+mkdir -p raw_data data_clean results
 
+# 2. Download data (only if missing)
+if [ ! -f raw_data/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz ]; then
+    echo "Downloading data..."
+    wget -P raw_data https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz
+else
+    echo "Data already exists"
+fi
+
+# 3. Convert VCF to PLINK format
 plink --vcf raw_data/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz \
       --make-bed \
       --out raw_data/chr22_raw
 
-# 2. Quality Control (QC)
-
+# 4. Quality Control (QC)
 plink --bfile raw_data/chr22_raw \
-      --maf 0.05 \ 
+      --maf 0.05 \
       --hwe 1e-6 \
       --geno 0.05 \
       --make-bed \
       --out data_clean/chr22_clean
 
-#3 Create Case Control
+# 5. Create Case-Control Phenotypes
+awk '{if (NR<=1253) print $1, $2, 1; else print $1, $2, 2}' \
+data_clean/chr22_clean.fam > data_clean/pheno_split.txt
 
-awk '{if (NR<=1253) print $1, $2, 1; else print $1, $2, 2}' data_clean/chr22_clean.fam > data_clean/pheno_split.txt
-
-#4 Integrate Phenotypes
-
+# 6. Integrate Phenotypes
 plink --bfile data_clean/chr22_clean \
       --pheno data_clean/pheno_split.txt \
       --make-bed \
       --out data_clean/chr22_final
 
-#Association Analysis
-#allow no sex because there is missing data for this column and the associations won't take place
+# 7. Association Analysis
 plink --bfile data_clean/chr22_final \
       --assoc \
       --allow-no-sex \
       --out results/gwas_result
 
-echo "Pipline Complete!"
-
+echo "Pipeline Complete!"
